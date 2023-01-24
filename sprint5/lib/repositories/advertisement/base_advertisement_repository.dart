aa4cdf49627f7fb969b142101a75498477844904
FeliciaChin <feliciac552@gmail.com>
2023-01-24 22:30:27 +0800

    hello

--- sprint5/lib/repositories/advertisement/base_advertisement_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:new_pro/blocs/advertisement/advertisement_bloc.dart';
-
-import '../../Page/models/models.dart';
-
-abstract class BaseadvertisementRepository {
-  Stream<List<Advertisement>> getAllAdvertisement();
-}
